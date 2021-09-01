56dd5ed555a27489e648451b7ed415b677e544bb
Valency Dickson <72242807+valencydickson@users.noreply.github.com>
2021-09-01 20:44:41 +0300

    Add initial files

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+include_once "header.php";
+
+?>
+
+<header class="container-fluid py-5">
+    <div class="d-flex justify-content-between align-items-center pb-4">
+        <a href="" class="heading text-light">TODO</a>
+        <img src="images/icon-moon.svg" alt="sun" class="theme-icon text-light">
+    </div>
+    <div class="form-group">
+        <input type="text" class="form-control" placeholder="Create a new todo">
+    </div>
+</header>
+
+<main class="container-fluid">
+    <div class="card">
+        <div class="card-body d-flex  align-items-center">
+            <img src="images/icon-check.svg" alt="check" class="border border-dark rounded-circle">
+            <span class="ml-3">Buy a neccessary things for kitchen</span>
+            <img src="images/icon-cross.svg" alt="cross" class="ml-auto">
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-body d-flex  align-items-center">
+            <img src="images/icon-check.svg" alt="check" class="border border-dark rounded-circle">
+            <span class="ml-3">Buy a neccessary things for kitchen</span>
+            <img src="images/icon-cross.svg" alt="cross" class="ml-auto">
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-body d-flex  align-items-center">
+            <img src="images/icon-check.svg" alt="check" class="border border-dark rounded-circle">
+            <span class="ml-3">Buy a neccessary things for kitchen</span>
+            <img src="images/icon-cross.svg" alt="cross" class="ml-auto">
+        </div>
+    </div>
+</main>
+
+</body>
+
+</html>
