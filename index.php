682ed089d8c63e2e3336dae71f2a2207d55612ec
Valency Dickson <72242807+valencydickson@users.noreply.github.com>
2021-09-02 13:40:33 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -15,30 +15,54 @@ include_once "header.php";
 </header>
 
 <main class="container-fluid">
-    <div class="card">
-        <div class="card-body d-flex  align-items-center">
-            <img src="images/icon-check.svg" alt="check" class="border border-dark rounded-circle">
-            <span class="ml-3">Buy a neccessary things for kitchen</span>
-            <img src="images/icon-cross.svg" alt="cross" class="ml-auto">
+
+    <div class="todos">
+        <div class="card">
+            <div class="card-body d-flex  align-items-center">
+                <img src="images/icon-check.svg" alt="check" class="border border-dark rounded-circle">
+                <span class="ml-3">Buy a neccessary things for kitchen</span>
+                <img src="images/icon-cross.svg" alt="cross" class="ml-auto">
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-body d-flex  align-items-center">
+                <img src="images/icon-check.svg" alt="check" class="border border-dark rounded-circle">
+                <span class="ml-3">Buy a neccessary things for kitchen</span>
+                <img src="images/icon-cross.svg" alt="cross" class="ml-auto">
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-body d-flex  align-items-center">
+                <img src="images/icon-check.svg" alt="check" class="border border-dark rounded-circle">
+                <span class="ml-3">Buy a neccessary things for kitchen</span>
+                <img src="images/icon-cross.svg" alt="cross" class="ml-auto">
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="card">
+            <div class="card-body d-flex justify-content-between  align-items-center">
+                <p>3 items left</p>
+                <p>Clear Completed</p>
+            </div>
         </div>
     </div>
 
-    <div class="card">
-        <div class="card-body d-flex  align-items-center">
-            <img src="images/icon-check.svg" alt="check" class="border border-dark rounded-circle">
-            <span class="ml-3">Buy a neccessary things for kitchen</span>
-            <img src="images/icon-cross.svg" alt="cross" class="ml-auto">
-        </div>
-    </div>
 
-    <div class="card">
-        <div class="card-body d-flex  align-items-center">
-            <img src="images/icon-check.svg" alt="check" class="border border-dark rounded-circle">
-            <span class="ml-3">Buy a neccessary things for kitchen</span>
-            <img src="images/icon-cross.svg" alt="cross" class="ml-auto">
-        </div>
-    </div>
 </main>
+
+<footer class="container-fluid mt-5">
+    <div class="card mb-5">
+        <div class="card-body d-flex justify-content-around  align-items-center">
+            <p>All</p>
+            <p>Active</p>
+            <p>Completed</p>
+        </div>
+    </div>
+    <p class="text-center mb-4">Drag and drop to reorder list</p>
+</footer>
 
 </body>
 
